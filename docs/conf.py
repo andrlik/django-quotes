ffdbeebc003fc46e3d6bbfa4583fa38731cecc89
Daniel Andrlik <daniel@andrlik.org>
2022-05-02 09:36:07 -0400

    Add support for markdown docs.

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosectionlabel",
+    "myst_parser"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
