859e4b90a621735db81edda7152dbbd6aac41218
Daniel Andrlik <daniel@andrlik.org>
2025-07-01 19:38:15 +0200

    Adds support for python 3.13 and django-markov 0.4 (#162)
    Also adds a --force option to the makemarkov command to enable
    easier model migration.

--- src/django_quotes/management/commands/makemarkov.py
+++ src/django_quotes/management/commands/makemarkov.py
@@ -1,24 +1,25 @@
-#
 # makemarkov.py
 #
-# Copyright (c) 2024 Daniel Andrlik
+# Copyright (c) 2024 - 2025 Daniel Andrlik
 # All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
-#
 
 """Checks if markov models are out of date, and if so regenerates them."""
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand  # type: ignore
 
-from django_markov.models import MarkovTextModel
-from django_quotes.models import SourceGroup
+from django_markov.models import MarkovTextModel  # type: ignore
+from django_quotes.models import SourceGroup  # type: ignore
 
 
 class Command(BaseCommand):
     help = "Checks if markov models are out of date, and if so regenerates them."
 
-    def handle(self, *args, **kwargs):
+    def add_arguments(self, parser):
+        parser.add_argument("--force", action="store_true", help="Update all models regardless of modification times.")
+
+    def handle(self, *args, **options):
         groups = SourceGroup.objects.all()
         groups_updated = 0
         characters_updated = 0
@@ -26,7 +27,7 @@ class Command(BaseCommand):
             if group.text_model is None:  # no cov
                 group.text_model = MarkovTextModel.objects.create()
             gmm = group.text_model
-            update_group = False
+            update_group = False if not options["force"] else True
             for character in group.source_set.filter(allow_markov=True):
                 if character.markov_ready:
                     quote_to_test = character.quote_set.all().order_by("-modified")[0]
@@ -35,7 +36,7 @@ class Command(BaseCommand):
                     cmm = character.text_model
                     if cmm.modified > gmm.modified:
                         update_group = True
-                    if quote_to_test.modified > cmm.modified:
+                    if quote_to_test.modified > cmm.modified or options["force"]:
                         character.update_markov_model()
                         characters_updated += 1
                         update_group = True
